da997cb651ea1eaf499e714dbc52993ac0c8c7dd
utkarshgupta14 <guptautkarsh1404@gmail.com>
2025-12-19 01:03:27 +0530

    finished adding basic functions

--- todoproj/todoapp/urls.py
+++ todoproj/todoapp/urls.py
@@ -5,5 +5,7 @@ urlpatterns=[
     path('', views.home, name='home-page'),
     path('register/', views.register, name='register'),
     path('login/', views.loginpage, name='login'),
-    path('logout/', views.logout, name='logout')
+    path('logout/', views.LogoutView, name='logout'),
+    path('delete-task/<str:name>/', views.DeleteTask, name='delete'),
+    path('update/<str:name>/', views.UpdateTask, name='update'),
 ]
--- todoproj/todoapp/views.py
+++ todoproj/todoapp/views.py
@@ -2,12 +2,30 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .models import todo
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def home(request):
-    return render(request, 'todoapp/todo.html', {})
+    if request.method == 'POST':
+        task = request.POST.get('task')
+        new_todo = todo(user=request.user, todo_name=task)
+        new_todo.save()
+        
+    todos = todo.objects.filter(user=request.user)
+    context = {
+        'todos':todos
+    }
+    return render(request, 'todoapp/todo.html', context)
+
+def LogoutView(request):
+    logout(request)
+    return redirect('login')
 
 def register(request):
+    if request.user.is_authenticated:
+        return redirect('home-page')
     if request.method == 'POST':
         username = request.POST.get('username')
         email = request.POST.get('email')
@@ -31,6 +49,8 @@ def register(request):
     return render(request, 'todoapp/register.html', {})
 
 def loginpage(request):
+    if request.user.is_authenticated:
+        return redirect('home-page')
     if request.method == 'POST':
         username = request.POST.get('uname')
         password = request.POST.get('pass')
@@ -44,3 +64,17 @@ def loginpage(request):
             return redirect('login')            
         
     return render(request, 'todoapp/login.html', {})
+
+@login_required
+def DeleteTask(request, name):
+    get_todo = todo.objects.get(user=request.user, todo_name=name)
+    if get_todo:
+        get_todo.delete()
+    return redirect('home-page')
+
+@login_required
+def UpdateTask(request, name):
+    get_todo = todo.objects.get(user=request.user, todo_name=name)
+    get_todo.status = not get_todo.status
+    get_todo.save()
+    return redirect('home-page')
